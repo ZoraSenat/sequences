b191f4ebb7f3d26d737ad5019e12601afbfdc65e
zora <ssh>
2016-10-28 10:26:12 -0500

    Added response to list of guesses

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,27 +1,29 @@
 class  GuessesController < ApplicationController
 
   def index
-
     #  Parameters: {"first_number"=>"1", "second_number"=>"2", "third_number"=>"3"}
+    g = Guess.new
+    g.first_num = params["first_number"]
+    g.second_num = params["second_number"]
+    g.third_num = params["third_number"]
+    g.save
 
-    @first_number = params["first_number"]
-    @second_number = params["second_number"]
-    @third_number = params["third_number"]
+    @list = Guess.all
+    # store numbers in db
+    #
+    # if @first_number.to_i < @second_number.to_i && @second_number.to_i < @third_number.to_i
+    #   @response = "Yes!"
+    # else
+    #   @response = "No."
 
-    if @first_number < @second_number && @second_number < @third_number
-      @response = "Yes!"
-    else
-      @response = "No."
-
-      render("guesses/index.html.erb")
-    end
-  end
+  render("guesses/index.html.erb")
+end
 
 
-  def answer
-    # Parameters: {"rule"=>"TESTING"}
+def answer
+  # Parameters: {"rule"=>"TESTING"}
 
-    @user_answer = params["rule"]
+  @user_answer = params["rule"]
 
-  end
+end
 end
